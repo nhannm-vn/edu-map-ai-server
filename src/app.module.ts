59af49b760cf61e4f876b2cb98ab66fe25ae4bf8
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2026-09-14 13:02:41 +0700

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './modules/auth/auth.module'
 import { PrismaModule } from 'prisma/prisma.module'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './modules/users/users.module'
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
